81bb0e855eb11fd1c1e70509cf26a9297a46c296
Sanchit Ingale <89060338+sanchit339@users.noreply.github.com>
2024-05-06 17:03:48 +0530

    Update to_parquet.py
    "To Parquet file"

--- to_parquet.py
+++ to_parquet.py
@@ -1,20 +1,14 @@
 import pandas as pd
 
-# Path to your dataset
-file_path = 'Dat.txt'
-
-# Initialize a list to hold each record as a single string
+file_path = 'Data.txt'
 records = []
 
-# Read the dataset line by line
 with open(file_path, 'r') as file:
     for line in file:
         records.append(line.strip())
 
-# Convert list to DataFrame with a single column named 'data'
 df = pd.DataFrame(records, columns=['data'])
 
-# Save the DataFrame as a Parquet file
 df.to_parquet('new_data.parquet', index=False)
 
 print("Data has been written to 'output.parquet'")
